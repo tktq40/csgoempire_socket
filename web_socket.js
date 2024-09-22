48099ef65f454bafdcf3eb0e30050bed687eec30
tktq40 <tktrungquoc40@gmail.com>
2024-09-22 15:12:30 +0700

    finish ws code

--- web_socket.js
+++ web_socket.js
@@ -1,8 +1,83 @@
-const socket = require('iov2');
+const iov2 = require('iov2');
 const csgoempire = require('@api/csgoempire')
+
+const empire_api_key = "";
 
 init_ws();
 
+
 async function init_ws() {
-      
+
+    csgoempire.auth(`Bearer ${empire_api_key}`);
+
+    const metadata = await csgoempire.metadata().then(res => {
+        return res.data;
+    }).catch(err => {
+        console.log('Error when getting metadata :', err);
+        return;
+    });
+    if (!metadata?.user) {
+        console.log(`Can't get metadata, abort !`);
+        return;
+    };
+
+    const socket = iov2(`wss://trade.csgoempire.com/trade`, {
+        transports: ["websocket"],
+        path: "/s/",
+        secure: true,
+        rejectUnauthorized: false,
+        reconnect: false,
+        //we will use recursive call to reconnect, if you want to auto-reconnect please write with your own logic + delete init_ws in line 81
+        extraHeaders: {
+            'User-agent': `API Bot`
+        },
+        query: {
+            uid: metadata.user.id,
+            token: metadata.socket_token
+        }
+    });
+    socket.on('connect', () => {
+        console.log('Connected to the websocket');
+    });
+    socket.on('init', async (data) => {
+        if (!data?.authenticated) {
+            socket.emit('identify', {
+                uid: metadata.user.id,
+                model: metadata.user,
+                authorizationToken: metadata.socket_token,
+                signature: metadata.socket_signature
+            });
+        };
+        socket.emit('filters', {
+            price_max: 99999999
+        });
+    });
+    // Event listeners
+    socket.on('new_item', (data) => {
+        console.log('New item:', data);
+    });
+
+    socket.on('updated_item', (data) => {
+        console.log('Updated item:', data);
+    });
+
+    socket.on('auction_update', (data) => {
+        console.log('Auction update:', data);
+    });
+
+    socket.on('deleted_item', (data) => {
+        console.log('Deleted item:', data);
+    });
+
+    socket.on('trade_status', (data) => {
+        console.log('Trade status:', data);
+    });
+
+    socket.on('deposit_failed', (data) => {
+        console.error('Deposit failed:', data);
+    });
+    socket.on('disconnect', (data) => {
+        console.log("Empire socket disconnect : ", data);
+        setTimeout(init_ws, 5000); // retry after 5 seconds
+    });
 };
